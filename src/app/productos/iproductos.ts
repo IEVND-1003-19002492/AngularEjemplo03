46238ce11a01c10a6aa45f8f008599ebfcf0106a
Diego <diego.4braham@gmail.com>
2023-09-22 21:11:26 -0600

    DAAT 22-9-23

---
+++ src/app/productos/iproductos.ts
@@ -0,0 +1,9 @@
+export interface IProductos {
+    "Modelo":string,
+    "Descripcion":string,
+    "Year": string,
+    "Precio":number,
+    "Marca": string,
+    "Color": string,
+    "ImagenUrl": string
+}
